cfd939b2793a86e9ee92980a31594a8b71083657
AtharvPS13 <fake34811289@gmail.com>
2025-07-13 20:28:51 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
 
 function App() {
   
 
   return (
-    <>
-     
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
