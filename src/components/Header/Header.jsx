77f1f48a57b1b2e9ba94be127a3f03fb5644697f
AtharvPS13 <fake34811289@gmail.com>
2025-07-19 16:42:18 +0530

    Updated Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import './Header.css';
+import cart from '../../assets/cart.png';
+import user from '../../assets/user.png';
+
 
 function Header() {
   return (
-    <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/canteen">Canteen</NavLink>
-    </>
+    <div className='navbar'>
+      <NavLink to="/" className="nav-link">Home</NavLink>
+      <NavLink to="/about" className="nav-link">About</NavLink>
+      <NavLink to="/canteen" className="nav-link">Canteen</NavLink>
+      <img src={user} alt='user-img' className='user-img'></img>
+      <img src={cart} alt='cart-img' className='cart-img'></img>
+    </div>
   )
 }
 
